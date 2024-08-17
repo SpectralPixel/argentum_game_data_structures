2755ee4498a7e3949c12ad75d249e4ff1846b525
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-17 19:51:58 +0200

    Implemented `new` method on `VoxelGrid`

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,35 @@
+use std::num::NonZero;
+
+use argentum_game_coordinate_system::region::SizeType;
 use argentum_game_voxel::Voxel;
-use ndarray::Array3;
+use ndarray::{Array3, Ix3};
 
 pub struct VoxelGrid {
+    size: SizeType,
     data: Array3<Voxel>
+}
+
+impl VoxelGrid {
+    pub fn new(size: NonZero<SizeType>) -> Self {
+        let size = size.get();
+        let s = usize::from(size);
+        Self {
+            size,
+            data: Array3::from_elem(Ix3(s, s, s), Voxel::default())
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use quickcheck::quickcheck;
+
+    use super::*;
+
+    quickcheck! {
+        fn new(size: NonZero<SizeType>) -> bool {
+            let _ = VoxelGrid::new(size);
+            true
+        }
+    }
 }
