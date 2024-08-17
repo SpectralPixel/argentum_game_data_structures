9d144771af14cb89e60bd1e8dfbaa9d4664e6f82
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-17 14:53:00 +0200

    Created skeleton for `VoxelGrid` data structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use argentum_game_voxel::Voxel;
+use ndarray::Array3;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub struct VoxelGrid {
+    data: Array3<Voxel>
 }
