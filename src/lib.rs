feeba83209cd3aaa866a674e7170a2ce1dc73c5d
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-17 20:17:19 +0200

    Created `ndarray` module and moved all code into it

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,4 @@
-use std::num::NonZero;
+//! This crate provides wrappers for data structures provided by dependencies. Each module corresponds to one dependency.
+mod ndarray;
 
-use argentum_game_coordinate_system::region::SizeType;
-use argentum_game_voxel::Voxel;
-use ndarray::{Array3, Ix3};
-
-pub struct VoxelGrid {
-    size: SizeType,
-    data: Array3<Voxel>
-}
-
-impl VoxelGrid {
-    pub fn new(size: NonZero<SizeType>) -> Self {
-        let size = size.get();
-        let s = usize::from(size);
-        Self {
-            size,
-            data: Array3::from_elem(Ix3(s, s, s), Voxel::default())
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use quickcheck::quickcheck;
-
-    use super::*;
-
-    quickcheck! {
-        fn new(size: NonZero<SizeType>) -> bool {
-            let _ = VoxelGrid::new(size);
-            true
-        }
-    }
-}
+pub use ndarray::VoxelGrid;
